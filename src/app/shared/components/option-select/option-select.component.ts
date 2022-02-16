17c8e92852ccd94a1a573d98e04f763201678fa3
fredneyparra0 <fredneyparra0@gmail.com>
2022-02-16 15:06:00 -0400

    options changed from formControlName

--- src/app/shared/components/option-select/option-select.component.ts
+++ src/app/shared/components/option-select/option-select.component.ts
@@ -1,12 +1,16 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { ControlContainer, FormGroupDirective } from '@angular/forms';
 
 type typeOption = 'position' | 'countrie';
 
 @Component({
   selector: 'app-option-select',
   templateUrl: './option-select.component.html',
-  styleUrls: ['./option-select.component.css']
+  styleUrls: ['./option-select.component.css'],
+  viewProviders: [
+		{ provide: ControlContainer, useExisting: FormGroupDirective }
+	]
 })
 export class OptionSelectComponent implements OnInit {
 
@@ -14,6 +18,7 @@ export class OptionSelectComponent implements OnInit {
   @Input() valueOptions: string[] = [];
   @Input() typeForm: string = '';
   @Input() typeOption: string = '';
+  @Input() controlName: string = '';
 
   constructor( private http: HttpClient ) { }
 
