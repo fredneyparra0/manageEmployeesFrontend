9d19298d4471a24e299d8eb6c51a0d696341840a
fredneyparra0 <fredneyparra0@gmail.com>
2022-02-17 14:51:12 -0400

    delete Employee completed

--- src/app/shared/components/btn-action/btn-action.component.ts
+++ src/app/shared/components/btn-action/btn-action.component.ts
@@ -1,4 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { EmployeeService } from 'src/app/employees/services/employee.service';
+ 
 
 type type = 'see' | 'edit' | 'delete' | 'create';
 
@@ -13,10 +16,23 @@ export class BtnActionComponent implements OnInit {
   @Input() type: type = 'see';
   @Input() url: string = '';
   @Input() route: string = '';
+  @Input() paramRoute:  string = '';
 
-  constructor() { }
+  constructor( 
+    private router: Router,
+    private serviceEmployee: EmployeeService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  onClikDelete() {
+    this.serviceEmployee.deleteOneEmployee(parseFloat(this.paramRoute))
+      .subscribe((e: any) => {
+        console.log(e);
+      })
+    
+    console.log(this.paramRoute)
   }
 
 }
